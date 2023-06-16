21c79dbbe9d209c281e87f523dcae7addddc45c9
Ely Lucas <ely@meta-tek.net>
2023-06-16 12:51:43 -0600

    update project id to point to docs instance

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 import { createHero, deleteHero } from './cypress/support/data';
 
 export default defineConfig({
-  projectId: 'nd8nd1',
+  projectId: 'x2youg',
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
